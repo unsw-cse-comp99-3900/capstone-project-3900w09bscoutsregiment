58713e4d1c1ed33070dc23de5ba33e303ff54863
Dylan Propert <dylanpropert63@gmail.com>
2024-06-30 12:36:33 +1000

    Move course endpoints from index to new router

--- backend/index.js
+++ backend/index.js
@@ -36,41 +36,7 @@ app.get('/', (req, res) => {
 // localhost:{PORT}/api/auth/signup or localhost:{PORT}/api/auth/login
 app.use('/api/auth', authRouter);
 
-app.post('/add-course', async (req, res) => {
-  const userId = req.body.userId;
-  if (userId == null) {
-    // we have a problem here i think
-    throw new Error('Need a user to add to');
-  }
-  const userExists = (await User.exists({_id: userId}).exec()) != null;
-  if (!userExists) {
-    throw new Error('User id doesn\'t exist in db');
-  }
-  const courseId = req.body.courseId;
-  if (courseId == null) {
-    // we also have a problem here
-    throw new Error('Need a course to add to the user');
-  }
-  const courseExists = (await Course.exists({_id: courseId}).exec()) != null;
-  if (!courseExists) {
-    throw new Error('User id doesn\'t exist in db');
-  }
-  const userHasCourse = (await HasCourse.exists({userId: userId, courseId: courseId}).exec()) != null;
-  if (userHasCourse) {
-    throw new Error('User should not have the same one twice');
-  }
-  const result = await HasCourse.create({
-    userId: userId,
-    courseId: courseId,
-    favorite: false
-  });
-  console.log(res);
-  res.send('ok');
-});
-
-app.post('/delete-course', async (req, res) => {
-
-});
+app.use('/api/course', courseRouter);
 
 db.once('open', async () => {
   console.log('Connected to MongoDB');
