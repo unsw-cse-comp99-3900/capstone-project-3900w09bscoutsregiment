c2baba4d64ddafeaf7e3b2ab93ebe0f611770d75
Leo-Zarni01 <leokhantzarni@gmail.com>
2024-07-15 20:35:40 +1000

    added form validation

--- frontend/src/app/feedback/page.jsx
+++ frontend/src/app/feedback/page.jsx
@@ -1,10 +1,62 @@
 'use client'; // needed for useState to work
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Link from 'next/link';
 
 export default function sendFeedback() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [name, setName] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [feedbackError, setFeedbackError] = useState(false);
   const form = useRef();
+
+  const handleEmailBlur = (e) => {
+    if (e.target.validity.typeMismatch || e.target.value === '') {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    if (event.target.validity.valid) {
+      setEmailError(false);
+    }
+  };
+
+  const handleNameBlur = (e) => {
+    if (e.target.value === '') {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    if (event.target.value !== '') {
+      setNameError(false);
+    }
+  };
+
+  const handleFeedbackBlur = (e) => {
+    if (e.target.value === '') {
+      setFeedbackError(true);
+    } else {
+      setFeedbackError(false);
+    }
+  };
+
+  const handleFeedbackChange = (event) => {
+    setFeedback(event.target.value);
+    if (event.target.value !== '') {
+      setFeedbackError(false);
+    }
+  };
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -39,7 +91,7 @@ export default function sendFeedback() {
         <form
           name="publish-form"
           id="form"
-          className=" w-1/3 justify-center mx-auto pt-3 relative top-12"
+          className=" w-1/3 justify-center mx-auto pt-3 relative top-10"
           ref={form}
           onSubmit={sendEmail}
         >
@@ -52,36 +104,63 @@ export default function sendFeedback() {
               Send us a feedback
             </label>
             <input
+              required
               name="user_email"
               className="shadow appearance-none border rounded w-full py-2 px-2 placeholder-blue-400 leading-tight focus:outline-none focus:shadow-outline"
               id="email-address"
               type="email"
               placeholder="Email address"
+              onChange={handleEmailChange}
+              onBlur={handleEmailBlur}
+              value={email}
             />
+            {emailError && (
+              <p role="alert" className="text-red-600 font-bold">
+                Please make sure you've entered an <em>email address</em>
+              </p>
+            )}
           </div>
 
           {/* name */}
           <div className="mb-4">
             <input
+              required
               color="#ff0000"
               name="user_name"
               className="shadow appearance-none border border-500 rounded w-full py-2 px-2 placeholder-blue-400 my-0"
               id="name"
               type="name"
               placeholder="Name"
+              onBlur={handleNameBlur}
+              onChange={handleNameChange}
+              value={name}
             />
+            {nameError && (
+              <p role="alert" className="text-red-600 font-bold">
+                Please make sure you've entered a <em>name</em>
+              </p>
+            )}
           </div>
 
           {/* feedback form */}
           <div className="mb-4">
             <textarea
+              required
               color="#ff0000"
               name="message"
               className="shadow appearance-none border border-500 rounded w-full h-32  py-2 px-2 placeholder-blue-400 my-0"
               id="feedback"
               type="text"
               placeholder="Enter your feedback here"
+              onBlur={handleFeedbackBlur}
+              onChange={handleFeedbackChange}
+              value={feedback}
             />
+            {feedbackError && (
+              <p role="alert" className="text-red-600 font-bold">
+                Please make sure you've entered a <em>feedback</em>
+              </p>
+            )}
           </div>
 
           {/* submit */}
