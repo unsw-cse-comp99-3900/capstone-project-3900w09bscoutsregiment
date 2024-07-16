e3e8d7f6f82e2ecbb3c97f9803e7b70ecf8f84b2
christopher <z5373462@ad.unsw.edu.au>
2024-07-16 21:09:27 +1000

    merge with main , and fixed  feedback page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -80,7 +80,7 @@ export default function Home() {
         <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       </div>
       <div>
-        <button className="flex justify-start mt-4 px-6 py-2 bg-primary-theme-lb hover:bg-blue-700 text-secondary-txt italic bg-primary-theme-lb font-semibold rounded-lg">Feedback</button>
+        <Link href={"/feedback"} className="flex justify-start mt-4 px-6 py-2 bg-primary-theme-lb hover:bg-blue-700 text-secondary-txt italic bg-primary-theme-lb font-semibold rounded-lg">Feedback</Link>
       </div>
       <div className='text-main-txt'>Want to help improve our app? Share your feedback!</div>
       <FooterPage />
