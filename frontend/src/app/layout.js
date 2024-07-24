73ab9265eca2b5beccca8dfddb0d9f9a527c0b3b
WhiteChn <cocochckn62@gmail.com>
2024-07-24 11:41:33 +1000

    Delete unnecessary or unused HTML code from dasboard/layout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -15,19 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-          <div className="px-16 py-4 text-white w-full flex items-center justify-between absolute">
-            <Link href='/' className='flex items-center gap-2'>
-              <div className='bg-white rounded-md w-8 h-8'></div>
-              <span className='text-white text-xl font-bold'>COTAM</span>
-            </Link>
-            <div>
-              <Link href='/login' className='outline_btn me-2'>Login</Link>
-              <Link href='/register' className='highlight_btn'>Register</Link>
-            </div>
-          </div>
           {children}
         </ThemeProvider>
-
       </body>
     </html>
   );
