f7256687c7d81aa4fff8198a7aae749ba7cf0ac0
Dylan <dylanpropert63@gmail.com>
2024-07-30 14:28:50 +1000

    add some stuff for a login test

--- backend/test/routes.test.js
+++ backend/test/routes.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import jwt from 'jsonwebtoken';
 import { describe, it } from 'node:test';
 import app from '../index.js';
 
@@ -6,6 +7,21 @@ describe('"/" route', () => {
   request(app)
     .get('/')
     .expect(200);
+});
+
+describe('login test', () => {
+  const agent = request.agent(app);
+
+  it('should login', () => {
+    agent
+    .post('/api/auth/login')
+    .send({ email: 'tester@gmail.com', password: 'tester' })
+    .expect('Content-Type', /json/)
+    .expect(200);
+  });
+});
+
+describe('login test', () => {
 });
 
 setTimeout(() => {
